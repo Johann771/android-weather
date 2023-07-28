a0570e664b027b1e22f93d1acf59ab94d65fe269
johannweytens <weytensjohann771@gmail.com>
2023-07-28 15:17:49 +0200

    init

--- app/src/main/java/fr/johann/android/androidweatherapp/activities/MainActivity.java
+++ app/src/main/java/fr/johann/android/androidweatherapp/activities/MainActivity.java
@@ -1,9 +1,15 @@
 package fr.johann.android.androidweatherapp.activities;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -12,10 +18,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.MutableLiveData;
 import fr.johann.android.androidweatherapp.R;
 import fr.johann.android.androidweatherapp.databinding.ActivityMainBinding;
 import fr.johann.android.androidweatherapp.models.City;
+import fr.johann.android.androidweatherapp.utils.UtilAPI;
 import okhttp3.*;
 import org.json.JSONException;
 
@@ -25,9 +34,23 @@ import java.io.IOException;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-
     private Context mContext;
+    private OkHttpClient mOkHttpClient;
     private City mCity;
+    private LocationManager mLocationManager;
+    private final int REQUEST_CODE = 1;
+    private Location mCurrentLocation;
+    private Handler mHandler;
+    private LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            mCurrentLocation = location;
+            Log.d("lol", "onLocationChanged: " + location);
+            // Récupération des données pour les coordonnées gps
+            updateWeatherDataCoordinates();
+            mLocationManager.removeUpdates(mLocationListener);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,23 +58,96 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         mContext = this;
+        mOkHttpClient = new OkHttpClient();
+        mHandler = new Handler();
 
         ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
 
         if (networkInfo != null && networkInfo.isConnected()) {
             binding.linearLayoutBackground.setVisibility(View.VISIBLE);
-            callApiWithCoords(39,0.6);
+            updateWeatherDataCoordinatesFromMyLocation();
         } else {
             binding.linearLayoutBackground.setVisibility(View.INVISIBLE);
+            updateViewError(R.string.no_connexion);
         }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission Granted
+                    updateWeatherDataCoordinatesFromMyLocation();
+                } else {
+                    // Permission Denied
+                    Toast.makeText(MainActivity.this, "Location Permission Denied", Toast.LENGTH_SHORT).show();
+                    // Permission Denied
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    public void updateWeatherDataCoordinatesFromMyLocation() {
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+        } else {
+            mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocationListener);
+        }
+    }
 
     public void onButtonClick(View view) {
         Intent intent = new Intent(mContext, FavoriteActivity.class);
         intent.putExtra("key_message", binding.mEditTextInput.getText().toString());
         startActivity(intent);
+    }
+
+    public void updateWeatherDataCoordinates() {
+
+        //String[] params = {String.valueOf(LAT), String.valueOf(LNG)};
+        String[] params = {String.valueOf(mCurrentLocation.getLatitude()), String.valueOf(mCurrentLocation.getLongitude())};
+
+
+        String s = String.format(UtilAPI.OPEN_WEATHER_MAP_API_COORDINATES, params);
+
+        Log.d("TAG", "updateWeatherDataCoordinates: "+s);
+        Request request = new Request.Builder().url(s).build();
+        mOkHttpClient.newCall(request).enqueue(new Callback() {
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+
+                final String stringJson = response.body().string();
+
+                if (response.isSuccessful() && UtilAPI.isSuccessful(stringJson)) {
+                    mHandler.post(() -> renderCurrentWeather(stringJson));
+                } else {
+                    mHandler.post(() -> updateViewError(R.string.place_not_found));
+                }
+            }
+        });
+    }
+
+    private void renderCurrentWeather(String jsonString) {
+
+        try {
+            mCity = new City(jsonString);
+            binding.setCity(mCity);
+            binding.linearLayoutBackground.setVisibility(View.VISIBLE);
+
+        } catch (JSONException e) {
+            updateViewError(R.string.api_error);
+        }
     }
 
     View.OnClickListener onClickListenerButton2 = new View.OnClickListener() {
@@ -131,5 +227,15 @@ public class MainActivity extends AppCompatActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+
+    public void updateViewError(int resString) {
+        binding.linearLayoutBackground.setVisibility(View.INVISIBLE);
+//
+//                        mLinearLayoutMain.setVisibility(View.INVISIBLE);
+//                        mProgressBarMain.setVisibility(View.INVISIBLE);
+//                        mFloatingButtonFavorite.setVisibility(View.INVISIBLE);
+//                        mTextViewNoConnection.setVisibility(View.VISIBLE);
+//                        mTextViewNoConnection.setText(resString);
     }
 }
